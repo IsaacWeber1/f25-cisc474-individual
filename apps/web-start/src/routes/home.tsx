2d1aa2589f36ddf48317d94fa8cf3c27f248c8bf
Isaac <isaacgweber@gmail.com>
2025-10-23 20:10:23 -0400

    fix: import order in home.tsx and regenerate Prisma client

--- apps/web-start/src/routes/home.tsx
+++ apps/web-start/src/routes/home.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { Link, createFileRoute } from '@tanstack/react-router';
 import { useAuth0 } from '@auth0/auth0-react';
 import { LogoutButton } from '../components/auth/LogoutButton';
 
